62159bab4244d795cc7ea35f40e73c1a4b23f9e4
pandorasNox <itspandorasnox@gmail.com>
2020-08-21 18:01:22 +0200

    add isbn tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        // assert_eq!(2 + 2, 4);
+        let maybe_isbn = crate::isbn::Isbn::from_string("".to_string());
+        assert!(maybe_isbn.is_err(), "empty isbn results in error");
+        assert!(!maybe_isbn.is_ok());
+        assert!(!maybe_isbn.is_ok() && maybe_isbn.is_err());
+
+        let maybe_isbn = crate::isbn::Isbn::from_string("123456789X".to_string());
+        assert!(!maybe_isbn.is_err(), "valid isbn");
+        assert!(maybe_isbn.is_ok());
+        assert!(maybe_isbn.is_ok() && !maybe_isbn.is_err());
+
+        assert!(
+            crate::isbn::Isbn::from_string("1-2-3-4-5-6-7-8-9-X".to_string()).is_ok(),
+            "valid isbn with dashes",
+        );
+        assert!(
+            crate::isbn::Isbn::from_string("1-2-3-4-5-6-7-8-9-X".to_string()).is_ok(),
+            "allow upper case X",
+        );
+        assert!(
+            crate::isbn::Isbn::from_string("1-2-3-4-5-6-7-8-9-x".to_string()).is_ok(),
+            "allow lower case x",
+        );
+        assert!(
+            crate::isbn::Isbn::from_string("1-2-3-4-5-6-7-8-8-x".to_string()).is_err(),
+            "ten letters but invalid isbn",
+        );
+    }
+}
+
 pub mod isbn {
     pub enum Isbn {
         Isbn10(String),
@@ -5,6 +39,9 @@ pub mod isbn {
     
     impl Isbn {
         pub fn from_string(maybe_isbn:String) -> Result<Isbn, String> {
+            let maybe_isbn = maybe_isbn.replace("-", "").to_string();
+            print!("DEBBUG: {}", maybe_isbn);
+
             if maybe_isbn.chars().count() != 10 {
                 let err = format!("isbn has to be 10 characters long, you currently got {}", maybe_isbn.chars().count());
                 return Err(err.to_string());
